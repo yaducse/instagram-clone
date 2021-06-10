f1583bbf7925a37a9bb7be81ab1898dc9fbcff3a
yaducse <yadubniketham@gmail.com>
2021-06-10 21:55:51 +0530

    responsive trial (index.html) + comments ascending

--- src/Post.js
+++ src/Post.js
@@ -17,7 +17,7 @@ function Post({ postId, username, user, caption, imageUrl }) {
         .collection('posts')
         .doc(postId)
         .collection('comments')
-        .orderBy('timestamp', 'desc')
+        .orderBy('timestamp', 'asc')
         .onSnapshot(snapshot => {
           setComments(
             snapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }))
